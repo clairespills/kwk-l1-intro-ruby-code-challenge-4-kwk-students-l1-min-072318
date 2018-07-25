94aeeb96dcde65bdd46fe64746c310876ca708a1
clairespills <purpleprincess372@gmail.com>
2018-07-25 20:56:42 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,20 +1,9 @@
 def least_coins(cents)
- coins_used = {"quarters"=>"1"}
-  until cents==0
-   if cents>=25
-    cents-=25
-    coins_used["quarters".to_i]=1
-   elsif cents>=10
-   cents-=10
-   coins_used["dimes".to_i]=1
-   elsif cents>=5
-   cents-=5
-   coins_used["dimes".to_i]=1
-   else
-   cents-=1
-   coins_used["pennys".to_i]=1
-end
-end
-print coins_used
-end
-least_coins(45)
+ coins_used = {:quarters =>0,:dimes =>0, :nickles => 0, :pennies =>0}
+  quarters=cents / 25
+   if quarters==0 
+     coins[:quarters]=0
+   elsif quarters!=0
+    coins[:quarters]=quarters
+    cents=cents-quarters*25
+  end
