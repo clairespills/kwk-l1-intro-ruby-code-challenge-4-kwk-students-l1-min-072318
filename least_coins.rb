ef04dd4ce0bfe70aa8740256ac0863b2f1ac56f2
clairespills <purpleprincess372@gmail.com>
2018-07-25 20:40:11 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,20 @@
-#write out your code here
-
 def least_coins(cents)
-
-#Code your answer here!
-
+ coins_used = []
+  until cents==0
+  if cents>=25
+    cents-=25
+    coins_used["quarters".to_i]=1
+  elsif cents>=10
+   cents-=10
+   coins_used["dimes".to_i]=1
+  elsif cents>=5
+   cents-=5
+   coins_used["dimes".to_i]=1
+  elsif
+   cents-=1
+   coins_used["pennys".to_i]=1
 end
+end
+puts coins_used
+end
+least_coins(45)
